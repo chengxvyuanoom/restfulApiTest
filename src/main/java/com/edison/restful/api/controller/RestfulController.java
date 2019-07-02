c7a292293c0258ffd3bf2889ac5fe8ddc6cae3d8
goodman <490466922@qq.com>
2019-07-02 19:02:08 +0800

    first commit

--- src/main/java/com/edison/restful/api/controller/RestfulController.java
+++ src/main/java/com/edison/restful/api/controller/RestfulController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * @CLassName ProductController
@@ -33,26 +34,30 @@ public class RestfulController {
 
   @ApiOperation(value = "接受普通GET请求(无任何限制),返回值为String类型，返回值内容与传入一致")
   @RequestMapping(value = "/get", method = RequestMethod.GET)
-  public String testForGet(@RequestParam String content, HttpServletRequest request) {
+  public String testForGet(@RequestParam String content,
+      @RequestParam(required = false) @ApiIgnore HttpServletRequest request) {
     return content;
   }
 
   @ApiOperation(value = "接受普通POST请求(无任何限制),返回值为String类型，返回值内容与传入一致")
   @RequestMapping(value = "/post", method = RequestMethod.POST)
-  public String testForPost(@RequestBody String content, HttpRequest request) {
+  public String testForPost(@RequestBody String content,
+      @RequestParam(required = false) @ApiIgnore HttpRequest request) {
     return content;
   }
 
   @ApiOperation(value = "接受普通PUT请求(无任何限制),返回值为String类型，返回值内容与传入一致")
   @RequestMapping(value = "/put", method = RequestMethod.PUT)
-  public String testForPut(@RequestBody String content, HttpRequest request) {
+  public String testForPut(@RequestBody String content,
+      @RequestParam(required = false) @ApiIgnore HttpRequest request) {
     return content;
   }
 
   @ApiOperation(value = "接受普通路径中的携带参数(2)(无任何限制),返回值为String类型，返回值内容是传入参数的List")
   @RequestMapping(value = "/path/{params1}/{params2}", method = RequestMethod.GET)
   public List<String> testForPath(@PathVariable(value = "params1") String params1,
-      @PathVariable(value = "params2") String params2, HttpRequest request) {
+      @PathVariable(value = "params2") String params2,
+      @RequestParam(required = false) @ApiIgnore HttpRequest request) {
     List<String> objects = Lists.newArrayList();
     objects.add(params1);
     objects.add(params2);
@@ -60,17 +65,18 @@ public class RestfulController {
   }
 
   @ApiOperation(value = "接受普通路径中的携带fgf参数(1)(无任何限制),返回值为String类型，返回值内容是传入参数的List")
-  @RequestMapping(value = "/path/{params1}}", method = RequestMethod.GET)
-  public String testForPath1(@PathVariable(value = "params1") String params1, HttpRequest request) {
+  @RequestMapping(value = "/path/{params1}", method = RequestMethod.GET)
+  public String testForPath1(@PathVariable(value = "params1") String params1,
+      @RequestParam(required = false) @ApiIgnore HttpRequest request) {
     return params1;
   }
 
   @ApiOperation(value = "接受普通Get请求，返回值为携带自定义Header内容,Header的Key，通过Url传过来，例如 XXXX?key=id")
   @RequestMapping(value = "/header/get", method = RequestMethod.GET)
-  public String testForHeader(@RequestParam(value = "key") String key, HttpRequest request) {
+  public String testForHeader(@RequestParam(value = "key") String key,
+      @RequestParam(required = false) @ApiIgnore HttpRequest request) {
     HttpFields headers = request.getHeaders();
     return headers.get(key);
   }
-
 
 }
